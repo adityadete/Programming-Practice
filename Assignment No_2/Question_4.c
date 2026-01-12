0ad3322d1ab54cab6c6ff808d2552fafb61b5446
adityadete <adityadete@gmail.com>
2026-01-12 19:31:37 +0530

    Assignment No_2

---
+++ Assignment No_2/Question_4.c
@@ -0,0 +1,29 @@
+//4. Accept two numbers from user and display first number in second
+//number of times.
+
+#include<stdio.h>
+void Display(int iNo, int ifreq)
+{
+    int iCnt = 0;
+    for(iCnt = 1; iCnt<=ifreq; iCnt++)
+    {
+        printf(" %d ",iNo);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    int iCount = 0;
+
+    printf("Enter number: ");
+    scanf("%d",&iValue);
+
+    printf("Enter frequency: ");
+    scanf("%d",&iCount);
+
+
+    Display(iValue,iCount);
+
+    return 0;
+}
---
+++ Assignment No_2/Question_5.c
@@ -0,0 +1,39 @@
+//5. Accept number from user and check whether number is even or
+//odd.
+
+#include<stdio.h>
+#include<stdbool.h>
+
+bool ChkEven(int iNo)
+{
+    if(iNo%2==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = true;
+
+    printf("Enter number: ");
+    scanf("%d",&iValue);
+
+    bRet = ChkEven(iValue);
+
+    if(bRet == true)
+    {
+        printf("Number is Even");
+    }
+    else
+    {
+        printf("Number is odd");
+    }
+
+    return 0;
+}
