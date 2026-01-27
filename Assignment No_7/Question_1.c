6a1622809086f24ffd0228ea0116f3cf1ee4e5dc
adityadete <adityadete@gmail.com>
2026-01-27 16:30:21 +0530

    Assignment No_7

---
+++ Assignment No_7/Question_1.c
@@ -0,0 +1,33 @@
+//      1.Write a program which accept number from user and display below pattern.
+
+#include <stdio.h>
+
+void Display(int iValue)
+{
+
+    if(iValue < 0)
+    {
+        iValue = -iValue;
+    }
+
+    for(int iCnt = 0; iCnt<=iValue; iCnt++)
+    {
+        printf("* ");
+    }
+    for(int iCnt = 0; iCnt<=iValue; iCnt++)
+    {
+        printf("# ");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
---
+++ Assignment No_7/Question_2.c
@@ -0,0 +1,24 @@
+//      2. Accept amount in US dollar and return its corresponding value in Indian currency.
+//      Consider 1$ as 70 rupees.
+
+#include <stdio.h>
+
+int Display(int iValue)
+{
+    return iValue * 70;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter a amount US dollar ($): ");
+    scanf("%d", &iValue);
+
+    iRet = Display(iValue);
+
+    printf("Amount in rupees: %d",iRet);
+
+    return 0;
+}
