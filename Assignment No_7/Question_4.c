35bf510304a39e52148ead9f1d5a3ef0783e0a1b
adityadete <adityadete@gmail.com>
2026-01-27 16:46:03 +0530

    Assignment No_7

---
+++ Assignment No_7/Question_4.c
@@ -0,0 +1,38 @@
+//      4. Write a program to find odd factorial of given number.
+
+#include<stdio.h>
+
+int Factorial(int iNo)
+{
+ 
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    int Fact = 1;
+
+    for(int iCnt = iNo; iCnt >= 1; iCnt--)
+    {
+        if(iCnt %2!=0)
+        {
+            Fact = Fact * iCnt;
+        }
+    }
+    return Fact;
+}
+
+int main()
+{
+
+    int iValue = 0, iRet = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+
+    iRet = Factorial(iValue);
+
+    printf("Odd Factorial of given Number is %d",iRet);
+
+    return 0;
+}
