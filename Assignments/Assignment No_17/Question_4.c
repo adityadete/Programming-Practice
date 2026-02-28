d8369d1b780a647ed76e32c39462034acbd9d4e0
Aditya Dete <adityadete7@gmail.com>
2026-02-28 23:04:23 +0530

    Assignment

---
+++ Assignments/Assignment No_17/Question_4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void pattern(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        if(iCnt % 2 != 0)
+        {
+            printf("%d ",iCnt);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+
+    pattern(iValue);
+
+
+    return 0;
+}
